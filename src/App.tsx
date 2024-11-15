12862749213a236298d0e70c94e1bdfed39612c7
javierluis15975@hotmail.com <javierluis15975@hotmail.com>
2024-11-14 21:51:18 -0500

    update node.js.yml and App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { useState } from 'react'
 import './App.css'
 import Bienvenido from './components/bienvenido'
 
 function App() {
+  const [count, setCount] = useState(0)
+
+  const add = () => setCount(count + 1)
+  const rest = () => setCount(count - 1)
+
   return (
     <>
       <h1>Hi docker compose and Dockerfile</h1>
@@ -11,6 +17,9 @@ function App() {
       <h5>Hola holasaxasxasdasdasdasdasdasx</h5>
       <h6>Bienvenido oyeee</h6>
       <Bienvenido />
+      <h1>Contador: {count}</h1>
+      <button onClick={add}>Agregar</button>
+      <button onClick={rest}>Restar</button>
     </>
   )
 }
